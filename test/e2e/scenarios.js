ea14637eac7ab65dfdeb11f29e023baad1c8f132
Anshul Dutta <anshuld@gmail.com>
2014-12-01 14:55:01 +0000

    Added filter support and some nice integration tests

--- test/e2e/scenarios.js
+++ test/e2e/scenarios.js
@@ -4,8 +4,43 @@
 
 describe('my app', function() {
 
-  beforeEach(function() {
-    browser.get('app/index.html');
+  describe('Phone list view', function() {
+
+    beforeEach(function() {
+      browser.get('app/index.html');
+    });
+
+    var query = element(by.model("query"));
+    
+    it("Should filter the list of phones based on the search criteria", function() {
+
+     var phoneList = element.all(by.repeater("phone in phones"));
+
+     expect(phoneList.count()).toBe(3);
+
+     query.sendKeys("nexus");
+     expect(phoneList.count()).toBe(1);
+
+     query.clear();
+     query.sendKeys("moto")
+     expect(phoneList.count()).toBe(2);
+
+     query.clear();
+     query.sendKeys("anshul")
+     expect(phoneList.count()).toBe(0);
+
+   });
+
+    it("Should have query in the title", function() {
+      
+      query.clear();
+      expect(browser.getTitle()).toMatch(/AKD with\s*/);
+
+      query.sendKeys('Jyoti Dutta');
+      expect(browser.getTitle()).toMatch(/AKD with\s+Jyoti Dutta/);
+
+
+    });
   });
 
 });
