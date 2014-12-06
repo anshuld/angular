1ddaf39e7770c7991d73d00423b85830ffab82a1
Anshul Dutta <anshuld@gmail.com>
2014-12-06 11:56:30 +0000

    Played with filters. built in as well custom created filter. Nice !!

--- test/unit/filtersSpec.js
+++ test/unit/filtersSpec.js
@@ -4,4 +4,13 @@
 
 describe('filter', function() {
 
+	describe('checkmark tests', function(){
+
+		beforeEach(module('phonecatFilters'));
+
+		it("should convert booleans to appropriate checkmarks", inject(function(checkmarkFilter) {
+			expect(checkmarkFilter(true)).toBe('\u2713');
+			expect(checkmarkFilter(false)).toBe('\u2718');
+		}));
+	});
 });
